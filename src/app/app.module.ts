c222cbe2c6326a3bce180de4ab8eda90b0d6373a
Daniel <fraxdev@gmail.com>
2020-05-07 20:49:26 +0300

    test

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http'; 
+import { StocksService } from './services/stocks.service';
+import { SummaryComponent } from './components/summary/summary.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ManageComponent } from './components/manage/manage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SummaryComponent,
+    DashboardComponent,
+    ManageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [StocksService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
